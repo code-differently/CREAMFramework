4d514e1ba0c58ae9398962cf86ff45a9b88ba03e
AakritiShresthaShrestha <Aakritibikash.stha88@gmail.com>
2021-01-21 13:54:37 -0500

    assign the behavior for the set interface

--- src/main/java/com/codedifferently/collections/Set.java
+++ src/main/java/com/codedifferently/collections/Set.java
@@ -1,5 +1,9 @@
 package com.codedifferently.collections;
 
 public interface Set {
-
+public void add();
+public void size();
+public void remove();
+public void contains();
+public void clear();
 }
