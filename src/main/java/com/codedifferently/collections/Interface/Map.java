dbbb549d67ce5aaced8982976084fe95215e9a76
AakritiShresthaShrestha <Aakritibikash.stha88@gmail.com>
2021-01-21 15:21:36 -0500

    Passed the arrguments in method or behavior of the interface

--- src/main/java/com/codedifferently/collections/Interface/Map.java
+++ src/main/java/com/codedifferently/collections/Interface/Map.java
@@ -1,12 +1,12 @@
 package com.codedifferently.collections.Interface;
 
 public interface Map<K,V> {
-    public void get(Object input);
-    public void put();
-    public void size();
-    public void containsKey();
-    public void containsValue();
-    public void isEmpty();
-    public void remove();
+    public V get(Object key);
+    public V put(K key, V value);
+    public int size();
+    public boolean containsKey(Object Key);
+    public boolean containsValue(Object Value);
+    public boolean isEmpty();
+    public V remove(Object key);
 
 }
