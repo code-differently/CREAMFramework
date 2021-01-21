30cd5f3f6c962618700f9dfb03a1334f9810f39a
AakritiShresthaShrestha <Aakritibikash.stha88@gmail.com>
2021-01-21 14:20:43 -0500

    assign the behavior for the linked interface

--- src/main/java/com/codedifferently/collections/List.java
+++ src/main/java/com/codedifferently/collections/List.java
@@ -1,4 +1,10 @@
 package com.codedifferently.collections;
 
-public class List {
+public interface List {
+    public void get();
+    public void add();
+    public void remove();
+    public void size();
+    public void contains();
+
 }
