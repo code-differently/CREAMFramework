6b4975b00167c4b0fa5ffabbf79dac403d2df73b
AakritiShresthaShrestha <Aakritibikash.stha88@gmail.com>
2021-01-21 15:02:54 -0500

    overriding the method in the concrete class

--- src/main/java/com/codedifferently/collections/Interface/Map.java
+++ src/main/java/com/codedifferently/collections/Interface/Map.java
@@ -1,11 +1,12 @@
 package com.codedifferently.collections.Interface;
 
-public interface Map<A> {
-    public void get(A input);
+public interface Map<K,V> {
+    public void get(Object input);
     public void put();
     public void size();
     public void containsKey();
     public void containsValue();
     public void isEmpty();
+    public void remove();
 
 }
--- src/main/java/com/codedifferently/collections/concreteClass/HashMap.java
+++ src/main/java/com/codedifferently/collections/concreteClass/HashMap.java
@@ -1,4 +1,55 @@
 package com.codedifferently.collections.concreteClass;
 
-public class HashMap {
+import java.util.Map;
+
+public class HashMap<K,V>  implements Map<K,V> {
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public boolean containsKey(Object key) {
+        return false;
+    }
+
+    @Override
+    public boolean containsValue(Object value) {
+        return false;
+    }
+
+    @Override
+    public V get(Object key) {
+        return null;
+    }
+
+//    @Override
+//    public V put(K key, V value) {
+//        return null;
+//    }
+
+    @Override
+    public V remove(Object key) {
+        return null;
+    }
+
+    @Override
+    public V put(K key, V value) {
+
+        return value;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+
+
+
 }
