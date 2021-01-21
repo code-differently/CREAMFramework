504019d133e61cf07dcad41d65045e0c8dd5823b
AakritiShresthaShrestha <Aakritibikash.stha88@gmail.com>
2021-01-21 13:51:49 -0500

    assign the behavior in map interface

--- src/main/java/com/codedifferently/collections/Map.java
+++ src/main/java/com/codedifferently/collections/Map.java
@@ -1,4 +1,11 @@
 package com.codedifferently.collections;
 
 public interface Map {
+    public void get();
+    public void put();
+    public void size();
+    public void containsKey();
+    public void containsValue();
+    public void isEmpty();
+
 }
--- src/main/java/com/codedifferently/collections/Set.java
+++ src/main/java/com/codedifferently/collections/Set.java
@@ -1,4 +1,5 @@
 package com.codedifferently.collections;
 
 public interface Set {
+
 }
