ed4720b2f5fd44267d8ac0a772a5b9f42240ff90
AakritiShresthaShrestha <Aakritibikash.stha88@gmail.com>
2021-01-26 21:39:41 -0500

    remove method of linked list is not passing and containsElement need to done

--- src/main/java/com/codedifferently/collections/concreteClass/LinkedList.java
+++ src/main/java/com/codedifferently/collections/concreteClass/LinkedList.java
@@ -2,34 +2,113 @@ package com.codedifferently.collections.concreteClass;
 
 import com.codedifferently.collections.Interface.List;
 
-public class LinkedList<E> implements List {
+public class LinkedList<E> implements List <E> {
+
+    private Node head;
+
     @Override
-    public Object getElementByIndex(int index) {
-        return null;
+    public E getElementByIndex(int index) {
+        Node currentNode = null;
+        //if index is lower than 1
+        if(index <0){
+            return null;
+        }
+        if(head != null){
+            currentNode=head.getNextNode();
+            for(int i=0;i<index;i++){
+                if(currentNode.getNextNode()==null){
+                    return null;
+                }
+                currentNode = currentNode.getNextNode();
+            }
+           // return currentNode.getData();
+        }
+        return (E) currentNode.getData();
     }
 
     @Override
     public void addElement(int index, Object element) {
+       // int counter =0;
 
+        if (head == null) {
+            head = new Node(element);
+        }
+
+        // inserts the specified element at the specified position in this list
+        Node tempNode = new Node(element);
+        Node currentNode = head;
+        // Let's check for NPE before iterate over crunchifyCurrent
+        if (currentNode != null) {
+            // crawl to the requested index or the last element in the list, whichever comes first
+            for (int i = 0; i < index && currentNode.getNextNode() != null; i++) {
+                currentNode = currentNode.getNextNode();
+            }
+        }
+        // set the new node's next-node reference to this node's next-node reference
+        currentNode.setNextNode(currentNode.getNextNode());
+        // now set this node's next-node reference to the new node
+        currentNode.setNextNode(tempNode);
+        // increment the number of elements variable
+        //counter++;
     }
+
 
     @Override
     public void removeElement(int index) {
-
+        Node currentNode = null;
+        int result =0;
+        int count=0;
+        //if index is lower than 1
+        if(index < 1 || index > sizeOfList()){
+            result=0;
+        }
+        if(head != null){
+            currentNode=head.getNextNode();
+            for(int i=0;i<index;i++){
+                if(currentNode.getNextNode()==null){
+                    result= 0;
+                }
+                currentNode = currentNode.getNextNode().getNextNode();
+            }
+            // return currentNode.getData();
+        }
+        count--;
     }
 
     @Override
     public int sizeOfList() {
-        return 0;
+        int count = 0;
+
+        if (head == null) {
+            return 0;
+        }
+
+
+        Node currentNode =head;
+
+      while(currentNode!=null){
+            currentNode = currentNode.getNextNode();
+
+            if(currentNode ==null) {
+            }else {
+                count++;
+            }
+        }
+        return count;
     }
+
 
     @Override
     public boolean containsElement(Object element) {
+
         return false;
     }
 
     @Override
     public boolean isListEmpty() {
+        if(sizeOfList()==0 ){
+            return true;
+        }
         return false;
     }
 }
--- src/test/java/com/codedifferently/collections/testListCase/LinkedListTest.java
+++ src/test/java/com/codedifferently/collections/testListCase/LinkedListTest.java
@@ -1,4 +1,128 @@
 package com.codedifferently.collections.testListCase;
 
+import com.codedifferently.collections.Interface.List;
+import com.codedifferently.collections.concreteClass.ArrayList;
+import com.codedifferently.collections.concreteClass.LinkedList;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class LinkedListTest {
+    List linkedList;
+
+    @Before
+    public void setup(){
+
+        linkedList = new LinkedList<Integer>();
+    }
+    @Test
+    public void getElementByIndexTest(){
+        String name1="Aakriti";
+        String name2="Amirah";
+        String name3="Linda";
+        String name4="Celita";
+        String name5="Lachelle";
+
+        linkedList.addElement(0,name1);
+        linkedList.addElement(1,name2);
+        linkedList.addElement(2,name3);
+        linkedList.addElement(3,name4);
+        linkedList.addElement(4,name5);
+
+        //when
+        String  actual= (String) linkedList.getElementByIndex(0);
+        String expected ="Aakriti";
+
+        //Then
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void addElementTest(){
+        String name1="Aakriti";
+        String name2="Amirah";
+        String name3="Linda";
+        String name4="Celita";
+        String name5="Lachelle";
+
+        linkedList.addElement(0,name1);
+        linkedList.addElement(1,name2);
+        linkedList.addElement(2,name3);
+        linkedList.addElement(3,name4);
+        linkedList.addElement(4,name5);
+
+        //when
+        int  actual=  linkedList.sizeOfList();
+        int expected =5;
+
+        //Then
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void addElementTest2(){
+//        String name1="Aakriti";
+//        String name2="Amirah";
+//        String name3="Linda";
+//        String name4="Celita";
+//        String name5="Lachelle";
+//
+//       linkedList.addElement(0,name1);
+//        linkedList.addElement(1,name2);
+//        linkedList.addElement(2,name3);
+//        linkedList.addElement(3,name4);
+//        linkedList.addElement(4,name5);
+
+        //when
+        int  actual=  linkedList.sizeOfList();
+        int expected =0;
+
+        //Then
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void isListEmptyTest(){
+        String name1="Aakriti";
+        String name2="Amirah";
+        String name3="Linda";
+        String name4="Celita";
+        String name5="Lachelle";
+
+        linkedList.addElement(0,name1);
+        linkedList.addElement(1,name2);
+        linkedList.addElement(2,name3);
+        linkedList.addElement(3,name4);
+        linkedList.addElement(4,name5);
+
+        //when
+        boolean  actual=  linkedList.isListEmpty();
+        boolean expected =false;
+
+        //Then
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void removeElementTest(){
+        String name1="Aakriti";
+        String name2="Amirah";
+        String name3="Linda";
+        String name4="Celita";
+        String name5="Lachelle";
+
+        linkedList.addElement(0,name1);
+        linkedList.addElement(1,name2);
+        linkedList.addElement(2,name3);
+        linkedList.addElement(3,name4);
+        linkedList.addElement(4,name5);
+
+        //when
+        linkedList.removeElement(0);
+        int  actual=  linkedList.sizeOfList();
+        int expected =4;
+
+        //Then
+        Assert.assertEquals(expected,actual);
+    }
 }
