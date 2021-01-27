2ded4ab3838a82d6c96fd39aaeff034df04e5b4d
Corban Weatherspoon <corbanweatherspoon@Corbans-MacBook-Air.local>
2021-01-26 20:28:39 -0500

    Updated

--- src/main/java/com/codedifferently/collections/list/LinkedList.java
+++ src/main/java/com/codedifferently/collections/list/LinkedList.java
@@ -1,24 +1,36 @@
 package com.codedifferently.collections.list;
 
-import com.codedifferently.collections.Collection;
-import com.codedifferently.collections.interfaces.List;
+import org.w3c.dom.Node;
 
 import javax.lang.model.element.Element;
 
-public class LinkedList<E> extends Collection implements List {
-    @Override
-    public void contains(Object o) {
+public class LinkedList<E> {
 
+    private LinkedList previous;
+    private String data;
+    private LinkedList next;
+
+    public LinkedList(String data, LinkedList next) {
+        this.data = data;
+        this.next = next;
     }
 
-    @Override
-    public void getIndex(int index) {
 
+    public String currentNode() {
+        return data + "";
     }
 
-    @Override
-    public void add(Element e) {
-
-
+    public String previousNode(String previous, String data) {
+        return previous + "";
     }
+
+    public String nextNode() {
+        return next + "";
+    }
+
+    public LinkedList addNode(String data, LinkedList newNode) {
+        LinkedList linked = new LinkedList(data, newNode);
+        return linked;
+    }
+
 }
--- src/test/java/com/codedifferently/collections/listTest/LinkedListTest.java
+++ src/test/java/com/codedifferently/collections/listTest/LinkedListTest.java
@@ -1,18 +1,29 @@
 package com.codedifferently.collections.listTest;
 
+import com.codedifferently.collections.list.ArrayList;
+import com.codedifferently.collections.list.LinkedList;
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.sql.SQLOutput;
 
 public class LinkedListTest {
 
-    @Test
-    public void contains() {
+@Test
+    public void currentNodeTest(){
+    LinkedList linkedList = new LinkedList( "Tim", null);
+    String expected = "Tim";
+    String actual = linkedList.currentNode();
+
+    Assert.assertEquals(expected, actual);
     }
 
-    @Test
-    public void getIndex() {
-    }
+@Test
+    public void addNode(){
+    LinkedList linky = new LinkedList("Tim", null);
+    LinkedList linked = new LinkedList("Paul", linky);
+    String expected = "Paul Tim";
+    String actual = linked.currentNode();
+}
 
-    @Test
-    public void add() {
-    }
 }
